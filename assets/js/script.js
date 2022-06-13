727166f2d021fcc3f1c3d31f64447a869224c951
malickbax <malickbax@gmail.com>
2022-06-13 11:49:03 -0400

    added drag and drop feature for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -177,3 +177,39 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+// $(".card .list-group").sortable({
+//   connectWith: $(".card .list-group")
+// });
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  // update: function(event) {
+  //   console.log("update", this);
+  // }
+  // update: function(event) {
+  //   console.log($(this).children());
+  // }
+  update: function(event) {
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      console.log($(this));
+    });
+  }
+});
+
