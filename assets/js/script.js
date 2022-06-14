ef5669beeb1a3d48ac61df35fc98266cf31383b3
malickbax <malickbax@gmail.com>
2022-06-14 10:20:33 -0400

    converted trash into a droppable zone and adjusted for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,4 +212,17 @@ $(".card .list-group").sortable({
     });
   }
 });
-
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
